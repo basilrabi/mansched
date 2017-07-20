bb80505d1de142253ae02a593deaa778e35d443c
basilrabi <ericbasil.rabi@gmail.com>
2017-07-21 03:30:06 +0800

    complete Employee-class tree

--- R/employee_class.R
+++ R/employee_class.R
@@ -51,7 +51,11 @@ NULL
 #'   Even if the employee will not report on duty, he or she will be paid by
 #'   this number of hours.
 #'   This slot only applies to real employees.
-#' @slot regularTime integer vector with length of 12
+#' @slot totHours integer vector with length of 12
+#'
+#'   This represents the number of hours per month which is equated to the
+#'   employee's basic monthly wage.
+#' @slot reg integer vector with length of 12
 #'
 #'   This represents the number of regular hours budgeted per month.
 #' @examples employee()
@@ -72,8 +76,8 @@ employee <- setClass(
     isTheo = 'logical',
     restday = 'character',
     holHours = 'integer',
-    # insert total payable reg hours per month
-    regularTime = 'integer'
+    totHours = 'integer',
+    reg = 'integer'
   )
 )
 
@@ -150,4 +154,247 @@ section_head <- setClass(
   'Section Head',
   slots = character(0),
   contains = 'Staff'
+)
+
+#' An S4 class representing an employee who is not a staff
+#'
+#' This class is entitled to premium pay for overtime work.
+#'   \code{Non Staff-class} is further classified into \code{\link{Clerk-class}}
+#'   and \code{\link{Operation Personnel-class}}.
+#'
+#' @slot isRF logical value \cr
+#'   Is the employee rank and file?
+#' @slot regOT integer vector with length of 12
+#'
+#'   This represents the number of regular overtime hours budgeted per month.
+#' @seealso \code{\link{Employee-class}}
+#' @examples non_staff()
+#' @export non_staff
+non_staff <- setClass(
+  'Non Staff',
+  slots = c(
+    isRF = 'logical',
+    regOT = 'integer'
+  ),
+  contains = 'Employee'
+)
+
+#' An S4 class representing a clerk
+#'
+#' Clerks are mainly responsible for data encoding. Prior to the creation of
+#'  \code{\link{mansched2}}, they were responsible for man hours encoding
+#'  using the spreadsheet template from accounting.
+#'
+#' @seealso \code{\link{Non Staff-class}}
+#' @examples clerk()
+#' @export clerk
+clerk <- setClass(
+  'Clerk',
+  slots = character(0),
+  contains = 'Non Staff'
+)
+
+#' An S4 class representing an operation personnel
+#'
+#' Operation-related personnel are classified under this class. These personnel
+#' have different scheduled rest days and thus have a different method of
+#' calculating premium for overtime work compared to \code{\link{Clerk-class}}.
+#' This class is further classified into \code{\link{Technical-class}} and
+#' \code{\link{Production Personnel-class}}.
+#'
+#' @slot rd integer vector with length of 12
+#'
+#'   This represents budgeted working hours of not more than 8 hours per day
+#'   rendered on a scheduled rest day. Each element of the vector represents a
+#'   month.
+#' @slot rdOT integer vector with length of 12
+#'
+#'   This represents budgeted working hours of more than 8 hours per day
+#'   rendered on a scheduled rest day. Each element of the vector represents a
+#'   month.
+#' @slot sh integer vector with length of 12
+#'
+#'   This represents budgeted working hours of not more than 8 hours per day
+#'   rendered on a special holiday. Each element of the vector represents a
+#'   month.
+#'
+#'   Only regular employees are entitled to a premium pay for this type of
+#'   holiday.
+#' @slot shOT integer vector with length of 12
+#'
+#'   This represents budgeted working hours of more than 8 hours per day
+#'   rendered on a special holiday. Each element of the vector represents a
+#'   month.
+#'
+#'   Only regular employees are entitled to a premium pay for this type of
+#'   holiday.
+#' @slot lh integer vector with length of 12
+#'
+#'   This represents budgeted working hours of not more than 8 hours per day
+#'   rendered on a legal holiday. Each element of the vector represents a
+#'   month.
+#' @slot lhOT integer vector with length of 12
+#'
+#'   This represents budgeted working hours of more than 8 hours per day
+#'   rendered on a legal holiday. Each element of the vector represents a
+#'   month.
+#' @slot nh integer vector with length of 12
+#'
+#'   This represents budgeted working hours of not more than 8 hours per day
+#'   rendered on a negotiated holiday. Each element of the vector represents a
+#'   month.
+#'
+#'   Only regular employees are entitled to a premium pay for this type of
+#'   holiday.
+#' @slot nhOT integer vector with length of 12
+#'
+#'   This represents budgeted working hours of more than 8 hours per day
+#'   rendered on a negotiated holiday. Each element of the vector represents a
+#'   month.
+#'
+#'   Only regular employees are entitled to a premium pay for this type of
+#'   holiday.
+#' @slot rs integer vector with length of 12
+#'
+#'   This represents budgeted working hours of not more than 8 hours per day
+#'   rendered on a special holiday and a rest day. Each element of the vector
+#'   represents a month.
+#'
+#'   Only regular employees are entitled to a compounded premium pay for this
+#'   type of holiday.
+#' @slot rsOT integer vector with length of 12
+#'
+#'   This represents budgeted working hours of more than 8 hours per day
+#'   rendered on a special holiday and a rest day. Each element of the vector
+#'   represents a month.
+#'
+#'   Only regular employees are entitled to a compounded premium pay for this
+#'   type of holiday.
+#' @slot rl integer vector with length of 12
+#'
+#'   This represents budgeted working hours of not more than 8 hours per day
+#'   rendered on a legal holiday and a rest day. Each element of the vector
+#'   represents a month.
+#' @slot rlOT integer vector with length of 12
+#'
+#'   This represents budgeted working hours of more than 8 hours per day
+#'   rendered on a legal holiday and a rest day. Each element of the vector
+#'   represents a month.
+#' @slot rn integer vector with length of 12
+#'
+#'   This represents budgeted working hours of not more than 8 hours per day
+#'   rendered on a negotiated holiday and a rest day. Each element of the vector
+#'   represents a month.
+#'
+#'   Only regular employees are entitled to a compounded premium pay for this
+#'   type of holiday.
+#' @slot rnOT integer vector with length of 12
+#'
+#'   This represents budgeted working hours of more than 8 hours per day
+#'   rendered on a negotiated holiday and a rest day. Each element of the vector
+#'   represents a month.
+#'
+#'   Only regular employees are entitled to a compounded premium pay for this
+#'   type of holiday.
+#' @seealso \code{\link{Non Staff-class}}
+#' @examples operation_personnel()
+#' @export operation_personnel
+operation_personnel <- setClass(
+  'Operation Personnel',
+  slots = c(
+    rd = 'integer',
+    rdOT = 'integer',
+    sh = 'integer',
+    shOT = 'integer',
+    lh = 'integer',
+    lhOT = 'integer',
+    nh = 'integer',
+    nhOT = 'integer',
+    rs = 'integer',
+    rsOT = 'integer',
+    rl = 'integer',
+    rlOT = 'integer',
+    rn = 'integer',
+    rnOT = 'integer'
+  ),
+  contains = 'Non Staff'
+)
+
+#' An S4 class representing a technical personnel
+#'
+#' Engineers, some draftsmen and assistants providing technical support are
+#'   classified under this S4 class. These personnel report on duty during the
+#'   day only.
+#'
+#' @seealso \code{\link{Operation Personnel-class}}
+#' @examples technical()
+#' @export technical
+technical <- setClass(
+  'Technical',
+  slots = character(0),
+  contains = 'Operation Personnel'
+)
+
+#' An S4 class representing a production personnel
+#'
+#' Production-related personnel are classified under this class. They may be
+#'   assigned during night shift, thus, they are entitled for a night premium
+#'   pay. This class is further classified into \code{\link{Supervisor-class}},
+#'   \code{\link{Laborer-class}} and \code{\link{Operator-class}}.
+#'
+#' @seealso \code{\link{Operation Personnel-class}}
+#' @examples production_personnel()
+#' @export production_personnel
+production_personnel <- setClass(
+  'Production Personnel',
+  slots = character(0),
+  contains = 'Operation Personnel'
+)
+
+#' An S4 class representing a supervisor
+#'
+#' This class may include field engineers and lead-men. Supervisors report to
+#'   their respective foremen or shift-in-charge. They are responsible for
+#'   monitoring their \code{\link{Laborer-class}} and
+#'   \code{\link{Operator-class}} personnel.
+#'
+#' @seealso \code{\link{Production Personnel-class}}
+#' @examples supervisor()
+#' @export supervisor
+supervisor <- setClass(
+  'Supervisor',
+  slots = character(0),
+  contains = 'Production Personnel'
+)
+
+#' An S4 class representing a laborer
+#'
+#' Samplers, checkers, spotters, plumbers and \emph{etc.} are classified under
+#'   this class.
+#'
+#' @seealso \code{\link{Production Personnel-class}}
+#' @examples laborer()
+#' @export laborer
+laborer <- setClass(
+  'Laborer',
+  slots = character(0),
+  contains = 'Production Personnel'
+)
+
+#' An S4 class representing an operator
+#'
+#' This class includes authorized heavy and light equipment operators and
+#'   drivers for trucks and services vehicles.
+#'
+#' @slot equipment character vector containing equipment types which the
+#'   employee is authorized to operate
+#' @seealso \code{\link{Production Personnel-class}}
+#' @examples operator()
+#' @export operator
+operator <- setClass(
+  'Operator',
+  slots = c(
+    equipment = 'character'
+  ),
+  contains = 'Production Personnel'
 )
---
+++ R/pkgname.R
@@ -0,0 +1,20 @@
+#' Tools for generating man hours database budget for TMC Mines Group
+#'
+#' This packages contains \code{R} functions that may help the computation of
+#'   man hours of TMC Mines Group personnel during budget season.
+#'
+#' Budget season in Taganito Mine means sleepless nights for its personnel,
+#'  especially the personnel from the Mine Engineering Department. Man hours
+#'  budgeting is a very tiresome task. This job requires estimating work
+#'  schedules, manpower requirements and lastly, manual encoding of
+#'  man hours into the spreadsheet template.
+#'
+#'  The spreadsheet template is full of details and its size is stupendous. It
+#'  has at least 12 sheets. Each sheet has more than a thousand rows and the
+#'  number of columns are magnitudes more than the number of rows.
+#'
+#'  Encoding the man hours into the template requires great amount of patience
+#'  and time. Also, finding encoding mistakes is also a discouraging task. This
+#'  package is an attempt to automate the task of manually encoding the man
+#'  hours data for manpower cost computation.
+'_PACKAGE'
