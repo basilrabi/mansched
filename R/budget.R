8a5abce7d40cdcb8e06aca9209006ca9c109444e
basilrabi <ericbasil.rabi@gmail.com>
2017-09-15 08:40:31 +0800

    remove support for ods files
    Only support xlsx files since all employees of TMC use MS Office.

--- R/budget.R
+++ R/budget.R
@@ -1,89 +1,15 @@
 #' Compute budget
 #'
-#' Given a spreadsheet containing the employee pool, requirements, wages, and
+#' Given a xlsx file containing the employee pool, requirements, wages, and
 #'   work schedules, the budget is computed.
 #'
 #' @param myFile full path of the spreadsheet
 #' @param year integer value defining the year to be budgeted
 #' @return NULL
 #' @export budget
-#' @importFrom readODS read_ods write_ods
-#' @importFrom readr col_character col_integer col_logical col_number cols
-budget <- function(myFile, year) {
-
-  # myFile <- "/home/brabi/sampleBudget/sampleData.ods"
-  # year <- 2018
-
-  empReq <- readODS::read_ods(
-    path = myFile,
-    sheet = 1,
-    col_types = readr::cols(.default = col_character(),
-                            quantity = col_integer(),
-                            spareFactor = col_number(),
-                            OT = col_integer()))
-
-  sched <- readODS::read_ods(
-    path = myFile,
-    sheet = 2,
-    col_types = readr::cols(.default = col_integer(),
-                            activity = col_character()))
-
-  empPool <- readODS::read_ods(
-    path = myFile,
-    sheet = 3,
-    col_types = readr::cols(.default = col_character(),
-                            attendance = col_number(),
-                            inHouse = col_logical(),
-                            isRF = col_logical()))
-
-  hol <- readODS::read_ods(
-    path = myFile,
-    sheet = 4,
-    col_types = readr::cols(Month = col_character(),
-                            Day = col_integer(),
-                            Type = col_character(),
-                            Description = col_character()))
-
-  tempData <- getmhDB(empReq = empReq,
-                      empPool = empPool,
-                      sched = sched,
-                      year = year,
-                      hol = hol)
-
-  mhDB <- tempData[[1]]
-  listR <- tempData[[3]]
-
-  wage <- readODS::read_ods(
-    path = myFile,
-    sheet = 5,
-    col_types = readr::cols(col_character(), col_number()))
-
-  export <- getCost(mhDB = mhDB, listR = listR, wage = wage)
-  tempFolder <- getwd()
-  dir.create(path = "./budget")
-  setwd(paste(tempFolder, "/budget", sep = ""))
-  for (i in export) {
-    readODS::write_ods(x = i[[2]],
-                       path = paste(i[[1]], ".ods", sep = ""))
-  }
-
-  setwd(tempFolder)
-
-  return(NULL)
-}
-
-#' Compute budget in windows
-#'
-#' Given a spreadsheet containing the employee pool, requirements, wages, and
-#'   work schedules, the budget is computed.
-#'
-#' @param myFile full path of the spreadsheet
-#' @param year integer value defining the year to be budgeted
-#' @return NULL
-#' @export budget2
 #' @importFrom readxl read_xlsx
 #' @importFrom xlsx write.xlsx
-budget2 <- function(myFile, year) {
+budget <- function(myFile, year) {
 
   # setwd("/home/brabi/sampleBudget/")
   # myFile <- "sampleData.xlsx"
@@ -176,8 +102,6 @@ budget2 <- function(myFile, year) {
     xlsx::write.xlsx(x = i[[2]],
                      file = paste(i[[1]], ".xlsx", sep = ""),
                      row.names = FALSE)
-
-    # print(as.data.frame(i[[2]]))
   }
 
   setwd(tempFolder)
