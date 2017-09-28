a40c9aa51deb5b4eb1a6c8c41e72a539ba1eb7df
basilrabi <ericbasil.rabi@gmail.com>
2017-09-28 11:22:06 +0800

    introduce mergeEmp()

---
+++ R/employee_fun_mergeEmp.R
@@ -0,0 +1,100 @@
+#' Merge theoretical employee list
+#'
+#' Combines all man hour types of a list of theoretical employees.
+#'   All man hours are stored in only one theoretical employee.
+#'
+#' @param x a list of \code{\link{Employee-class}} representing theoretical
+#'   employees with the same class and the same costCode slot
+#'
+#'   For \code{\link{Operator-class}}, the equipment slot must also be the same.
+#' @return an \code{\link{Employee-class}} object
+#' @export mergeEmp
+mergeEmp <- function(x) {
+
+  if(class(x) != "list")
+    stop("Argument is not a list of Employee-class objects!")
+
+  if (length(x) == 1L) {
+
+    if (is(x[[1]], "Employee")) {
+
+      return(x[[1]])
+
+    } else {
+
+      stop("Argument is not a list of Employee-class objects!")
+
+    }
+
+  }
+
+  if (!is(x[[1]], "Employee"))
+    stop("Argument is not a list of Employee-class objects!")
+
+  tempEmp <- x[[1]]
+
+  tempClass <- sapply(x, FUN = function(x) {class(x)})
+  tempClass <- unique(tempClass)
+  if (length(tempClass) > 1)
+    stop("Incompatible Employee-class!")
+
+  tempCostCode <- sapply(x, FUN = function(x) {x@costCode})
+  tempCostCode <- unique(tempCostCode)
+
+  if (length(tempCostCode) > 1)
+    stop("Incompatible cost code!")
+
+  tempEmp@ID <- paste(tempClass, tempCostCode, sep = "-")
+
+  if (is(x[[1]], "Operator")) {
+
+    tempEquip <- sapply(x, FUN = function(x) {x@equipment})
+    tempEquip <- unique(tempEquip)
+    if (length(tempEquip) > 1)
+      stop("Incompatible equipment!")
+
+    tempEmp@ID <- paste(tempEmp@ID, tempEquip, sep = "-")
+
+  }
+
+  zero <- rep(0L, times = 12)
+
+  reg <- sapply(x, FUN = function(y) {y@reg})
+  reg <- apply(reg, MARGIN = 1, FUN = sum)
+  tempEmp@reg <- reg
+
+  if (is(tempEmp, "Non Staff")) {
+
+    regOT <- sapply(x, FUN = function(y) {y@regOT})
+    regOT <- apply(regOT, MARGIN = 1, FUN = sum)
+    tempEmp@regOT <- regOT
+
+  }
+
+  if (is(tempEmp, "Operation Personnel")) {
+
+    sh <- sapply(x, FUN = function(y) {y@sh})
+    lh <- sapply(x, FUN = function(y) {y@lh})
+    nh <- sapply(x, FUN = function(y) {y@nh})
+    shOT <- sapply(x, FUN = function(y) {y@shOT})
+    lhOT <- sapply(x, FUN = function(y) {y@lhOT})
+    nhOT <- sapply(x, FUN = function(y) {y@nhOT})
+
+    sh <- apply(sh, MARGIN = 1, FUN = sum)
+    lh <- apply(lh, MARGIN = 1, FUN = sum)
+    nh <- apply(nh, MARGIN = 1, FUN = sum)
+    shOT <- apply(shOT, MARGIN = 1, FUN = sum)
+    lhOT <- apply(lhOT, MARGIN = 1, FUN = sum)
+    nhOT <- apply(nhOT, MARGIN = 1, FUN = sum)
+
+    tempEmp@sh <- sh
+    tempEmp@lh <- lh
+    tempEmp@nh <- nh
+    tempEmp@shOT <- shOT
+    tempEmp@lhOT <- lhOT
+    tempEmp@nhOT <- nhOT
+
+  }
+
+  return(tempEmp)
+}
---
+++ tests/testthat/test_mergeEmp.R
@@ -0,0 +1,205 @@
+library(mansched)
+
+spareFactor <- 1
+calDays <- getCalDays(cBegin = "2018-01-01",
+                      hol = getHol(hol = holidays, year = "2018"),
+                      restday = "Sunday")
+
+mdtProb <- getMDTProb(hol = getHol(hol = holidays, year = 2018))
+
+testDays <- c(25, 24, 27, 22, 25, 23, 25, 25, 24, 26, 24, 24)
+
+empListA <- list()
+empListB <- list()
+empListC <- list()
+empListD <- list()
+empListE <- list()
+empListF <- list()
+empListG <- list()
+empListH <- list()
+empListI <- list()
+
+empListA[[1]] <- createEmp("division manager")
+empListA[[2]] <- createEmp("department manager")
+
+empListB[[1]] <- createEmp("section head")
+empListB[[2]] <- createEmp("section head")
+empListB[[3]] <- createEmp("section head")
+
+empListB[[1]] <- initTEmployee(theObject = empListB[[1]],
+                               ID = "E1",
+                               costCode = "C01",
+                               calDays = calDays)
+empListB[[2]] <- initTEmployee(theObject = empListB[[2]],
+                               ID = "E1",
+                               costCode = "C01",
+                               calDays = calDays)
+empListB[[3]] <- initTEmployee(theObject = empListB[[3]],
+                               ID = "E1",
+                               costCode = "C02",
+                               calDays = calDays)
+
+empListC[[1]] <- createEmp("section head")
+empListC[[2]] <- createEmp("section head")
+empListC[[3]] <- createEmp("section head")
+
+empListC[[1]] <- initTEmployee(theObject = empListC[[1]],
+                               ID = "E1",
+                               costCode = "C01",
+                               calDays = calDays)
+empListC[[2]] <- initTEmployee(theObject = empListC[[2]],
+                               ID = "E1",
+                               costCode = "C01",
+                               calDays = calDays)
+empListC[[3]] <- initTEmployee(theObject = empListC[[3]],
+                               ID = "E1",
+                               costCode = "C01",
+                               calDays = calDays)
+
+empMergedC <- mergeEmp(empListC)
+
+empListD[[1]] <- createEmp("supervisor")
+empListD[[2]] <- createEmp("supervisor")
+empListD[[3]] <- createEmp("supervisor")
+
+empListD[[1]] <- initTEmployee(theObject = empListD[[1]],
+                               ID = "E1",
+                               costCode = "C01",
+                               mdtProb = mdtProb)
+empListD[[2]] <- initTEmployee(theObject = empListD[[2]],
+                               ID = "E1",
+                               costCode = "C01",
+                               mdtProb = mdtProb)
+empListD[[3]] <- initTEmployee(theObject = empListD[[3]],
+                               ID = "E1",
+                               costCode = "C02",
+                               mdtProb = mdtProb)
+
+empListD[[1]] <- createEmp("supervisor")
+empListD[[2]] <- createEmp("supervisor")
+empListD[[3]] <- createEmp("supervisor")
+empListE[[1]] <- createEmp("supervisor")
+empListE[[2]] <- createEmp("supervisor")
+empListE[[3]] <- createEmp("supervisor")
+
+
+empListD[[1]] <- initTEmployee(theObject = empListD[[1]],
+                               ID = "E1",
+                               costCode = "C01",
+                               mdtProb = mdtProb)
+empListD[[2]] <- initTEmployee(theObject = empListD[[2]],
+                               ID = "E1",
+                               costCode = "C01",
+                               mdtProb = mdtProb)
+empListD[[3]] <- initTEmployee(theObject = empListD[[3]],
+                               ID = "E1",
+                               costCode = "C02",
+                               mdtProb = mdtProb)
+
+empListE[[1]] <- initTEmployee(theObject = empListE[[1]],
+                               ID = "E1",
+                               costCode = "C01",
+                               mdtProb = mdtProb)
+empListE[[2]] <- initTEmployee(theObject = empListE[[2]],
+                               ID = "E1",
+                               costCode = "C01",
+                               mdtProb = mdtProb)
+empListE[[3]] <- initTEmployee(theObject = empListE[[3]],
+                               ID = "E1",
+                               costCode = "C01",
+                               mdtProb = mdtProb)
+
+empListF[[1]] <- createEmp("operator")
+empListF[[2]] <- createEmp("operator")
+empListF[[3]] <- createEmp("operator")
+empListG[[1]] <- createEmp("operator")
+empListG[[2]] <- createEmp("operator")
+empListG[[3]] <- createEmp("operator")
+empListH[[1]] <- createEmp("operator")
+empListH[[2]] <- createEmp("operator")
+empListH[[3]] <- createEmp("operator")
+empListI[[1]] <- createEmp("operator")
+empListI[[2]] <- createEmp("operator")
+empListI[[3]] <- createEmp("operator")
+
+empListF[[1]] <- initTEmployee(theObject = empListF[[1]],
+                               ID = "E1",
+                               costCode = "C01",
+                               equipment = "DT",
+                               mdtProb = mdtProb)
+empListF[[2]] <- initTEmployee(theObject = empListF[[2]],
+                               ID = "E1",
+                               equipment = "DT",
+                               costCode = "C01",
+                               mdtProb = mdtProb)
+empListF[[3]] <- initTEmployee(theObject = empListF[[3]],
+                               ID = "E1",
+                               costCode = "C02",
+                               equipment = "DT",
+                               mdtProb = mdtProb)
+
+empListG[[1]] <- initTEmployee(theObject = empListG[[1]],
+                               ID = "E1",
+                               costCode = "C01",
+                               equipment = "DT",
+                               mdtProb = mdtProb)
+empListG[[2]] <- initTEmployee(theObject = empListG[[2]],
+                               ID = "E1",
+                               equipment = "DT",
+                               costCode = "C01",
+                               mdtProb = mdtProb)
+empListG[[3]] <- initTEmployee(theObject = empListG[[3]],
+                               ID = "E1",
+                               costCode = "C01",
+                               equipment = "TX",
+                               mdtProb = mdtProb)
+
+empListH[[1]] <- initTEmployee(theObject = empListH[[1]],
+                               ID = "E1",
+                               costCode = "C01",
+                               equipment = "DT",
+                               mdtProb = mdtProb)
+empListH[[2]] <- initTEmployee(theObject = empListH[[2]],
+                               ID = "E1",
+                               equipment = "DT",
+                               costCode = "C01",
+                               mdtProb = mdtProb)
+empListH[[3]] <- initTEmployee(theObject = empListH[[3]],
+                               ID = "E1",
+                               costCode = "C02",
+                               equipment = "TX",
+                               mdtProb = mdtProb)
+
+empListI[[1]] <- initTEmployee(theObject = empListI[[1]],
+                               ID = "E1",
+                               costCode = "C01",
+                               equipment = "DT",
+                               mdtProb = mdtProb)
+empListI[[2]] <- initTEmployee(theObject = empListI[[2]],
+                               ID = "E1",
+                               equipment = "DT",
+                               costCode = "C01",
+                               mdtProb = mdtProb)
+empListI[[3]] <- initTEmployee(theObject = empListI[[3]],
+                               ID = "E1",
+                               costCode = "C01",
+                               equipment = "DT",
+                               mdtProb = mdtProb)
+
+test_that("mergeEmp() works", {
+  expect_error(mergeEmp(empListA))
+  expect_error(mergeEmp(empListB))
+  expect_equal(sum(getHours(empMergedC)),
+               sum(calDays$reg * 8) * length(empListC))
+  expect_equal(empMergedC@ID, "Section Head-C01")
+  expect_error(mergeEmp(empListD))
+  expect_equal(sum(getHours(mergeEmp(empListE))), getHoursL(empListE))
+  expect_equal(mergeEmp(empListE)@ID, "Supervisor-C01")
+  expect_equal(mergeEmp(empListE)@costCode, "C01")
+  expect_error(mergeEmp(empListF))
+  expect_error(mergeEmp(empListG))
+  expect_error(mergeEmp(empListH))
+  expect_equal(sum(getHours(mergeEmp(empListI))), getHoursL(empListI))
+  expect_equal(mergeEmp(empListI)@ID, "Operator-C01-DT")
+  expect_equal(mergeEmp(empListI)@costCode, "C01")
+})
