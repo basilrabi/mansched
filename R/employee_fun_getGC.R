004af0101af72ea7925d971462f117f821664359
basilrabi <ericbasil.rabi@gmail.com>
2018-09-20 00:12:48 +0800

    add getGC()

---
+++ R/employee_fun_getGC.R
@@ -0,0 +1,60 @@
+#' @import methods
+NULL
+
+#' Compute gift certificate cost
+#'
+#' Gift certificates amounting to 1500 PhP are usually given to employee on
+#'   December for non-seasonal employees. For seasonal employees, the gift
+#'   certificates are awarded in October.
+#'
+#' @param theObject \code{\link{Employee-class}} object
+#' @return a \code{\link{data.frame}} with 12 rows and 3 columns although
+#'   only either the 10th or 12th row may have a value since the cost is charged
+#'   only in October (for seasonal employees) and in December (for regular and
+#'   probationary employees).
+#'
+#'   Each row represents a month. The columns are:
+#'   \describe{
+#'      \item{ID}{character string representing the unique identifier of the
+#'        real employee}
+#'      \item{month}{integer value representing the month}
+#'      \item{gc}{numeric value defining the cost charged for that month}
+#'   }
+#' @export getGC
+setGeneric(
+  name = "getGC",
+  def  = function(theObject) {
+    standardGeneric("getGC")
+  }
+)
+
+#' @describeIn getGC Summarize gift certificate cost
+setMethod(
+  f          = "getGC",
+  signature  = "Employee",
+  definition = function(theObject) {
+
+    gc <- getCM(theObject)
+    gc$gc <- 0
+
+    if (!theObject@forecast) {
+
+      if (theObject@status %in% c("reg", "pro")) {
+        gc$gc[gc$month == 12L] <- 1500
+        gc$gc <- round(gc$allow * gc$gc, digits = 2)
+      }
+
+      if (theObject@status == "sea") {
+
+        if (sum(gc$allow) > 10) {
+          multiplier <- 10
+        } else {
+          multiplier <- sum(gc$allow)
+        }
+        gc$gc[gc$month == 10L] <- (1500 / 10) * multiplier
+      }
+    }
+
+    return(gc[, c("month", "ID", "gc")])
+  }
+)
---
+++ tests/testthat/test_getGC.R
@@ -0,0 +1,234 @@
+library(mansched)
+
+ID          <- "S-240"
+name        <- "Basil Eric Rabi"
+designation <- "Materials Engineering Supervisor"
+costCode    <- "14000"
+status      <- "reg"
+cBegin      <- "2012-10-15"
+inHouse     <- TRUE
+restday     <- "Tuesday"
+hol         <- getHol(hol = holidays, year = 2018)
+
+tempEmp  <- createEmp(empClass = "divisionmanager")
+tempEmp1 <- initREmployee(theObject   = tempEmp,
+                          ID          = ID,
+                          name        = name,
+                          designation = designation,
+                          costCode    = costCode,
+                          status      = status,
+                          cBegin      = cBegin,
+                          inHouse     = inHouse,
+                          restday     = restday,
+                          hol         = hol)
+
+cEnd   <- "2018-11-01"
+inHouse  <- FALSE
+tempEmp  <- createEmp(empClass = "groupmanager")
+tempEmp2 <- initREmployee(theObject   = tempEmp,
+                          ID          = ID,
+                          name        = name,
+                          designation = designation,
+                          costCode    = costCode,
+                          status      = status,
+                          cBegin      = cBegin,
+                          cEnd        = cEnd,
+                          inHouse     = inHouse,
+                          restday     = restday,
+                          hol         = hol)
+
+cBegin   <- "2018-03-01"
+inHouse  <- FALSE
+status   <- "pro"
+tempEmp  <- createEmp(empClass = "clerk")
+tempEmp3 <- initREmployee(theObject   = tempEmp,
+                          ID          = ID,
+                          name        = name,
+                          designation = designation,
+                          costCode    = costCode,
+                          status      = status,
+                          cBegin      = cBegin,
+                          inHouse     = inHouse,
+                          restday     = restday,
+                          hol         = hol,
+                          RF          = FALSE)
+
+cBegin   <- "2018-04-01"
+inHouse  <- TRUE
+status   <- "sea"
+tempEmp  <- createEmp(empClass = "technical")
+tempEmp4 <- initREmployee(theObject   = tempEmp,
+                          ID          = ID,
+                          name        = name,
+                          designation = designation,
+                          costCode    = costCode,
+                          status      = status,
+                          cBegin      = cBegin,
+                          inHouse     = inHouse,
+                          restday     = restday,
+                          hol         = hol)
+
+cBegin   <- "2018-01-01"
+inHouse   <- TRUE
+status    <- "sea"
+equipment <- "FL"
+tempEmp   <- createEmp(empClass = "operator")
+tempEmp5  <- initREmployee(theObject   = tempEmp,
+                           ID          = ID,
+                           name        = name,
+                           designation = designation,
+                           costCode    = costCode,
+                           status      = status,
+                           cBegin      = cBegin,
+                           inHouse     = inHouse,
+                           restday     = restday,
+                           hol         = hol,
+                           equipment   = equipment)
+
+test_that("getGC() works", {
+  expect_equal(sum(getGC(tempEmp1)$gc), 1500)
+  expect_equal(getGC(tempEmp1)$gc[12], 1500)
+  expect_equal(sum(getGC(tempEmp2)$gc), 0)
+  expect_equal(sum(getGC(tempEmp3)$gc), 1500)
+  expect_equal(getGC(tempEmp3)$gc[12], 1500)
+  expect_equal(sum(getGC(tempEmp4)$gc), 1350)
+  expect_equal(getGC(tempEmp4)$gc[10], 1350)
+  expect_equal(sum(getGC(tempEmp5)$gc), 1500)
+  expect_equal(getGC(tempEmp5)$gc[10], 1500)
+})
+
+# Test for forecast
+
+ID          <- "S-240"
+name        <- "Basil Eric Rabi"
+designation <- "Materials Engineering Supervisor"
+costCode    <- "14000"
+status      <- "reg"
+cBegin      <- "2012-10-15"
+inHouse     <- TRUE
+restday     <- "Tuesday"
+hol         <- getHol(hol = holidays, year = 2018)
+
+tempEmp  <- createEmp(empClass = "divisionmanager")
+tempEmp1 <- initREmployee(theObject   = tempEmp,
+                          ID          = ID,
+                          name        = name,
+                          designation = designation,
+                          costCode    = costCode,
+                          status      = status,
+                          cBegin      = cBegin,
+                          inHouse     = inHouse,
+                          restday     = restday,
+                          hol         = hol,
+                          forecast    = TRUE)
+
+cBegin   <- "2018-02-01"
+inHouse  <- FALSE
+tempEmp  <- createEmp(empClass = "groupmanager")
+tempEmp2 <- initREmployee(theObject   = tempEmp,
+                          ID          = ID,
+                          name        = name,
+                          designation = designation,
+                          costCode    = costCode,
+                          status      = status,
+                          cBegin      = cBegin,
+                          inHouse     = inHouse,
+                          restday     = restday,
+                          hol         = hol,
+                          forecast    = TRUE)
+
+cBegin   <- "2018-03-01"
+inHouse  <- FALSE
+tempEmp  <- createEmp(empClass = "clerk")
+tempEmp3 <- initREmployee(theObject   = tempEmp,
+                          ID          = ID,
+                          name        = name,
+                          designation = designation,
+                          costCode    = costCode,
+                          status      = status,
+                          cBegin      = cBegin,
+                          inHouse     = inHouse,
+                          restday     = restday,
+                          hol         = hol,
+                          RF          = FALSE,
+                          forecast    = TRUE)
+
+cBegin   <- "2018-04-01"
+inHouse  <- TRUE
+status   <- "sea"
+tempEmp  <- createEmp(empClass = "technical")
+tempEmp4 <- initREmployee(theObject   = tempEmp,
+                          ID          = ID,
+                          name        = name,
+                          designation = designation,
+                          costCode    = costCode,
+                          status      = status,
+                          cBegin      = cBegin,
+                          inHouse     = inHouse,
+                          restday     = restday,
+                          hol         = hol,
+                          forecast    = TRUE)
+
+inHouse   <- TRUE
+status    <- "sea"
+equipment <- "FL"
+tempEmp   <- createEmp(empClass = "operator")
+tempEmp5  <- initREmployee(theObject   = tempEmp,
+                           ID          = ID,
+                           name        = name,
+                           designation = designation,
+                           costCode    = costCode,
+                           status      = status,
+                           cBegin      = cBegin,
+                           inHouse     = inHouse,
+                           restday     = restday,
+                           hol         = hol,
+                           equipment   = equipment,
+                           forecast    = TRUE)
+
+cEnd     <- "2018-10-31"
+inHouse  <- TRUE
+status   <- "sea"
+tempEmp  <- createEmp(empClass = "technical")
+tempEmp6 <- initREmployee(theObject   = tempEmp,
+                          ID          = ID,
+                          name        = name,
+                          designation = designation,
+                          costCode    = costCode,
+                          status      = status,
+                          cBegin      = cBegin,
+                          cEnd        = cEnd,
+                          inHouse     = inHouse,
+                          restday     = restday,
+                          hol         = hol,
+                          forecast    = TRUE)
+
+cEnd     <- "2018-10-31"
+inHouse  <- TRUE
+status   <- "age"
+tempEmp  <- createEmp(empClass = "laborer")
+tempEmp7 <- initREmployee(theObject   = tempEmp,
+                          ID          = ID,
+                          name        = name,
+                          designation = designation,
+                          costCode    = costCode,
+                          status      = status,
+                          cBegin      = cBegin,
+                          cEnd        = cEnd,
+                          inHouse     = inHouse,
+                          restday     = restday,
+                          hol         = hol,
+                          forecast    = TRUE)
+
+
+test_that("getRiceSub() works", {
+  expect_equal(sum(getGC(tempEmp1)$gc), 0)
+  expect_equal(sum(getGC(tempEmp2)$gc), 0)
+  expect_equal(sum(getGC(tempEmp3)$gc), 0)
+  expect_equal(sum(getGC(tempEmp4)$gc), 0)
+  expect_equal(sum(getGC(tempEmp5)$gc), 0)
+  expect_equal(sum(getGC(tempEmp6)$gc), 0)
+  expect_equal(sum(getGC(tempEmp7)$gc), 0)
+})
+
+rm(list = ls())
