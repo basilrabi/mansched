988caf55fc26798ba936d059969164169bf9d7a5
basilrabi <ericbasil.rabi@gmail.com>
2018-09-20 01:21:23 +0800

    add getCBA()

---
+++ R/employee_fun_getCBA.R
@@ -0,0 +1,45 @@
+#' @import methods
+NULL
+
+#' Compute cost for CBA provisions
+#'
+#' Taganito Mine provides items to all regular employees as agreen upon with
+#'   the labor union.
+#'
+#' @param theObject \code{\link{Employee-class}} object
+#' @return a \code{\link{data.frame}} with 12 rows and 3 columns representing
+#'   the distributed cost of working long shirts per month
+#'
+#'   Each row represents a month. The columns are:
+#'   \describe{
+#'      \item{ID}{character string representing the unique identifier of the
+#'        real employee}
+#'      \item{month}{integer value representing the month}
+#'      \item{cba}{numeric value defining the cost charged for that month}
+#'   }
+#' @export getCBA
+setGeneric(
+  name = "getCBA",
+  def  = function(theObject) {
+    standardGeneric("getCBA")
+  }
+)
+
+#' @describeIn getCBA Compute pro-rated cost for CBA provisions
+#'   throughout the year
+setMethod(
+  f          = "getCBA",
+  signature  = "Employee",
+  definition = function(theObject) {
+
+    price <- 5300
+    cba <- getCM(theObject)
+
+    if (theObject@forecast | theObject@status != "reg")
+      price <- 0
+
+    cba$cba <- round(cba$allow * price / 12, digits = 2)
+
+    return(cba[, c("month", "ID", "cba")])
+  }
+)
---
+++ tests/testthat/test_getCBA.R
@@ -0,0 +1,262 @@
+library(mansched)
+
+ID          <- "S-240"
+name        <- "Basil Eric Rabi"
+designation <- "Materials Engineering Supervisor"
+costCode    <- "14000"
+status      <- "reg"
+cBegin      <- "2012-10-15"
+inHouse     <- TRUE
+restday     <- "Tuesday"
+hol         <- getHol(hol = holidays, year = 2018)
+
+tempEmp  <- createEmp(empClass = "divisionmanager")
+tempEmp1 <- initREmployee(theObject   = tempEmp,
+                          ID          = ID,
+                          name        = name,
+                          designation = designation,
+                          costCode    = costCode,
+                          status      = status,
+                          cBegin      = cBegin,
+                          inHouse     = inHouse,
+                          restday     = restday,
+                          hol         = hol)
+
+cBegin   <- "2018-02-01"
+inHouse  <- FALSE
+tempEmp  <- createEmp(empClass = "groupmanager")
+tempEmp2 <- initREmployee(theObject   = tempEmp,
+                          ID          = ID,
+                          name        = name,
+                          designation = designation,
+                          costCode    = costCode,
+                          status      = status,
+                          cBegin      = cBegin,
+                          inHouse     = inHouse,
+                          restday     = restday,
+                          hol         = hol)
+
+cBegin   <- "2018-03-01"
+inHouse  <- FALSE
+tempEmp  <- createEmp(empClass = "clerk")
+tempEmp3 <- initREmployee(theObject   = tempEmp,
+                          ID          = ID,
+                          name        = name,
+                          designation = designation,
+                          costCode    = costCode,
+                          status      = status,
+                          cBegin      = cBegin,
+                          inHouse     = inHouse,
+                          restday     = restday,
+                          hol         = hol,
+                          RF          = FALSE)
+
+cBegin   <- "2018-04-01"
+inHouse  <- TRUE
+status   <- "pro"
+tempEmp  <- createEmp(empClass = "technical")
+tempEmp4 <- initREmployee(theObject   = tempEmp,
+                          ID          = ID,
+                          name        = name,
+                          designation = designation,
+                          costCode    = costCode,
+                          status      = status,
+                          cBegin      = cBegin,
+                          inHouse     = inHouse,
+                          restday     = restday,
+                          hol         = hol)
+
+inHouse   <- TRUE
+status    <- "sea"
+equipment <- "FL"
+tempEmp   <- createEmp(empClass = "operator")
+tempEmp5  <- initREmployee(theObject   = tempEmp,
+                           ID          = ID,
+                           name        = name,
+                           designation = designation,
+                           costCode    = costCode,
+                           status      = status,
+                           cBegin      = cBegin,
+                           inHouse     = inHouse,
+                           restday     = restday,
+                           hol         = hol,
+                           equipment   = equipment)
+
+cEnd     <- "2018-10-31"
+inHouse  <- TRUE
+status   <- "sea"
+tempEmp  <- createEmp(empClass = "technical")
+tempEmp6 <- initREmployee(theObject   = tempEmp,
+                          ID          = ID,
+                          name        = name,
+                          designation = designation,
+                          costCode    = costCode,
+                          status      = status,
+                          cBegin      = cBegin,
+                          cEnd        = cEnd,
+                          inHouse     = inHouse,
+                          restday     = restday,
+                          hol         = hol)
+
+cEnd     <- "2018-10-31"
+inHouse  <- TRUE
+status   <- "age"
+tempEmp  <- createEmp(empClass = "laborer")
+tempEmp7 <- initREmployee(theObject   = tempEmp,
+                          ID          = ID,
+                          name        = name,
+                          designation = designation,
+                          costCode    = costCode,
+                          status      = status,
+                          cBegin      = cBegin,
+                          cEnd        = cEnd,
+                          inHouse     = inHouse,
+                          restday     = restday,
+                          hol         = hol)
+
+
+test_that("getCBA() works", {
+  expect_equal(sum(getCBA(tempEmp1)$cba), 441.67 * 12)
+  expect_equal(sum(getCBA(tempEmp2)$cba), 441.67 * 11)
+  expect_equal(sum(getCBA(tempEmp3)$cba), 441.67 * 10)
+  expect_equal(sum(getCBA(tempEmp4)$cba), 0)
+  expect_equal(sum(getCBA(tempEmp5)$cba), 0)
+  expect_equal(sum(getCBA(tempEmp6)$cba), 0)
+  expect_equal(sum(getCBA(tempEmp7)$cba), 0)
+})
+
+# Test for forecast
+
+ID          <- "S-240"
+name        <- "Basil Eric Rabi"
+designation <- "Materials Engineering Supervisor"
+costCode    <- "14000"
+status      <- "reg"
+cBegin      <- "2012-10-15"
+inHouse     <- TRUE
+restday     <- "Tuesday"
+hol         <- getHol(hol = holidays, year = 2018)
+
+tempEmp  <- createEmp(empClass = "divisionmanager")
+tempEmp1 <- initREmployee(theObject   = tempEmp,
+                          ID          = ID,
+                          name        = name,
+                          designation = designation,
+                          costCode    = costCode,
+                          status      = status,
+                          cBegin      = cBegin,
+                          inHouse     = inHouse,
+                          restday     = restday,
+                          hol         = hol,
+                          forecast    = TRUE)
+
+cBegin   <- "2018-02-01"
+inHouse  <- FALSE
+tempEmp  <- createEmp(empClass = "groupmanager")
+tempEmp2 <- initREmployee(theObject   = tempEmp,
+                          ID          = ID,
+                          name        = name,
+                          designation = designation,
+                          costCode    = costCode,
+                          status      = status,
+                          cBegin      = cBegin,
+                          inHouse     = inHouse,
+                          restday     = restday,
+                          hol         = hol,
+                          forecast    = TRUE)
+
+cBegin   <- "2018-03-01"
+inHouse  <- FALSE
+tempEmp  <- createEmp(empClass = "clerk")
+tempEmp3 <- initREmployee(theObject   = tempEmp,
+                          ID          = ID,
+                          name        = name,
+                          designation = designation,
+                          costCode    = costCode,
+                          status      = status,
+                          cBegin      = cBegin,
+                          inHouse     = inHouse,
+                          restday     = restday,
+                          hol         = hol,
+                          RF          = FALSE,
+                          forecast    = TRUE)
+
+cBegin   <- "2018-04-01"
+inHouse  <- TRUE
+status   <- "sea"
+tempEmp  <- createEmp(empClass = "technical")
+tempEmp4 <- initREmployee(theObject   = tempEmp,
+                          ID          = ID,
+                          name        = name,
+                          designation = designation,
+                          costCode    = costCode,
+                          status      = status,
+                          cBegin      = cBegin,
+                          inHouse     = inHouse,
+                          restday     = restday,
+                          hol         = hol,
+                          forecast    = TRUE)
+
+inHouse   <- TRUE
+status    <- "sea"
+equipment <- "FL"
+tempEmp   <- createEmp(empClass = "operator")
+tempEmp5  <- initREmployee(theObject   = tempEmp,
+                           ID          = ID,
+                           name        = name,
+                           designation = designation,
+                           costCode    = costCode,
+                           status      = status,
+                           cBegin      = cBegin,
+                           inHouse     = inHouse,
+                           restday     = restday,
+                           hol         = hol,
+                           equipment   = equipment,
+                           forecast    = TRUE)
+
+cEnd     <- "2018-10-31"
+inHouse  <- TRUE
+status   <- "sea"
+tempEmp  <- createEmp(empClass = "technical")
+tempEmp6 <- initREmployee(theObject   = tempEmp,
+                          ID          = ID,
+                          name        = name,
+                          designation = designation,
+                          costCode    = costCode,
+                          status      = status,
+                          cBegin      = cBegin,
+                          cEnd        = cEnd,
+                          inHouse     = inHouse,
+                          restday     = restday,
+                          hol         = hol,
+                          forecast    = TRUE)
+
+cEnd     <- "2018-10-31"
+inHouse  <- TRUE
+status   <- "age"
+tempEmp  <- createEmp(empClass = "laborer")
+tempEmp7 <- initREmployee(theObject   = tempEmp,
+                          ID          = ID,
+                          name        = name,
+                          designation = designation,
+                          costCode    = costCode,
+                          status      = status,
+                          cBegin      = cBegin,
+                          cEnd        = cEnd,
+                          inHouse     = inHouse,
+                          restday     = restday,
+                          hol         = hol,
+                          forecast    = TRUE)
+
+
+test_that("getCBA() works", {
+  expect_equal(sum(getCBA(tempEmp1)$cba), 0)
+  expect_equal(sum(getCBA(tempEmp2)$cba), 0)
+  expect_equal(sum(getCBA(tempEmp3)$cba), 0)
+  expect_equal(sum(getCBA(tempEmp4)$cba), 0)
+  expect_equal(sum(getCBA(tempEmp5)$cba), 0)
+  expect_equal(sum(getCBA(tempEmp6)$cba), 0)
+  expect_equal(sum(getCBA(tempEmp7)$cba), 0)
+})
+
+rm(list = ls())
